d081ee79f09a1f62e6a4a9919c6d6a5794669fb9
chsohn15 <chsohn15@gmail.com>
2024-11-14 23:15:07 -0500

    update my story

--- src/components/MyStory.js
+++ src/components/MyStory.js
@@ -24,15 +24,18 @@ const MyStory = (props) => {
               <Col md="8">
                 <Card.Body>
                   <Card.Text>
-                  <p>I am a full stack web developer with experience in Ruby and Javascript frameworks. Through my experience as a high school English teacher for five years, I crafted creative lessons and projects to appeal to a diversity of students and meet their learning goals. My detailed attention to the voice of the consumer, my love of language construction, and my interest in an intuitive and transformative user experience translate seamlessly into the coding world.   </p>
+                    <p>I am a full stack web developer with experience in CSS, Javascript, and PHP. During my time at Facing History & Ourselves, I worked with a team of developers and technical architects to transformed a Drupal 7 site into a new Digital Experience Platform in Drupal 10. In my daily work, I collaborate with UX/UI designers to create and implement intuitive user experiences for educators.</p>
+                    <p>Before my time at Facing History, I worked as a high school English teacher for five years, crafting creative lessons and projects to appeal to a diversity of students and meet their learning goals. My detailed attention to the voice of the consumer, my love of language construction, and my interest in an intuitive and transformative user experience translate seamlessly into the coding world.</p>
                     <p>When I transitioned to a software engineering student at Flatiron School coding bootcamp, I found another outlet for my creativity by building multifaceted projects using Ruby on Rails, Javascript, and React. My resourcefulness and hard work have been essential to ensuring that my projects are carefully planned, implemented, and edited.</p>
                     <pr>Here is a list of some of the technologies I have been using recently:</pr>
                 <ul>
+                <li>CSS/Sass</li>
+                <li>Javascript</li>
+                <li>Drupal</li>
+                <li>PHP</li>
                 <li>React</li>
                 <li>Redux</li>
-                <li>Javascript</li>
                 <li>Ruby on Rails</li>
-                <li>HTML and CSS</li>
                 <li>Material UI, Bootstrap UI, Semantic UI</li>
             </ul>
 
